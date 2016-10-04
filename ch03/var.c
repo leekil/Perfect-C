b2e13ac54d960ffc521d2c2397a3018dacebfa83
leekil <dlrlf369@naver.com>
2016-10-04 19:36:42 +0900

    소스파일
    단원 폴더와 소스 파일 추가

---
+++ ch03/var.c
@@ -0,0 +1,16 @@
+﻿#include	<stdio.h>
+
+int main(void)
+
+{
+	int snum;
+	int credits;
+
+	snum = 20163021;
+	credits = 18;
+
+	printf("ÇÐ¹ø: %d\n", snum);
+	printf("½ÅÃ»ÇÐÁ¡: %d\n", credits);
+
+	return 0;
+}
